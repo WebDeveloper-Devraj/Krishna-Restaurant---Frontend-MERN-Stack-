aebd7abc45190cc5741139b7f03af66f8c4122d5
Devraj Pujari <devrajpujari292@gmail.com>
2025-05-05 23:16:39 +0530

    Fix: Correct import path case for AdminLayout.module.css

--- src/admin/AdminLayout.jsx
+++ src/admin/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom"; // Outlet will render the nested routes
-import styles from "./adminLayout.module.css";
+import styles from "./AdminLayout.module.css";
 import AdminHeader from "./AdminHeader";
 import Dashboard from "./Dashboard";
 
